f33b3d4113cfa5a48f8a85e7ca6d0c4e893ac5db
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-01-22 12:28:09 +0530

    22 Jan 2k23

---
+++ twoSumCodeStudio.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+vector<pair<int, int>> twoSum(vector<int> &arr, int target, int n)
+{
+    // Write your code here.
+    pair<int, int> p;
+    vector<pair<int, int>> ans;
+    sort(arr.begin(), arr.end());
+    int s = 0, e = n - 1;
+    while (s < e)
+    {
+        if (arr[s] + arr[e] == target)
+        {
+            p.first = arr[s];
+            p.second = arr[e];
+            ans.push_back(p);
+            s++;
+            e--;
+        }
+        else if (arr[s] + arr[e] > target)
+        {
+            e--;
+        }
+        else
+            s++;
+    }
+    if (ans.empty())
+    {
+        ans.push_back({-1, -1});
+        return ans;
+    }
+    return ans;
+}
