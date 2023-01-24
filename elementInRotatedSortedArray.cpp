f3c2ff9a71dd1b36da9183481d66dbeea612e9ad
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-01-24 22:02:38 +0530

    24 jan 2023

---
+++ elementInRotatedSortedArray.cpp
@@ -0,0 +1,56 @@
+// Approach1 T.s - O(log n)
+
+int getPivot(vector<int> &arr, int n)
+{
+    int s = 0, e = n - 1, mid = s + (e - s) / 2;
+    while (s < e)
+    {
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+int binarySearch(vector<int> &arr, int s, int e, int key)
+{
+    int start = s;
+    int end = e;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+
+#include <bits/stdc++.h>
+int findPosition(vector<int> &arr, int n, int k)
+{
+    int pivot = getPivot(arr, n);
+    if (k >= arr[pivot] && k <= arr[n - 1])
+    {
+        return binarySearch(arr, pivot, n - 1, k);
+    }
+    else
+    {
+        return binarySearch(arr, 0, pivot - 1, k);
+    }
+}
