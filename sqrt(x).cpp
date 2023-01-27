98204cf08574387789852b3adc33d41d4c36e908
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-01-27 23:02:44 +0530

    27 jan 2023

---
+++ sqrt(x).cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int binarySearch(int n)
+    {
+        int s = 0, e = n, ans = -1;
+        long long int mid = s + (e - s) / 2;
+        while (s <= e)
+        {
+            long long int square = mid * mid;
+            if (square > n)
+            {
+                e = mid - 1;
+            }
+            else if (square < n)
+            {
+                ans = mid;
+                s = mid + 1;
+            }
+            else if (square == n)
+            {
+                return mid;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
+    int mySqrt(int x)
+    {
+        return binarySearch(x);
+    }
+};
