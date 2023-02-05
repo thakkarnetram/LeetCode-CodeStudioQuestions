c70c2a4687559225a2d30327558f0db04f18765d
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-02-05 11:29:14 +0530

    5Feb 2023

--- bubbleSortCodeStudio.cpp
+++ bubbleSortCodeStudio.cpp
@@ -3,7 +3,7 @@ void bubbleSort(vector<int>& arr, int n)
 {   
    for(int i =0;i<n-1;i++){
        for(int j =0; j<n-i-1;j++){
-           if(arr[j]>arr[j+1]){
+           if(arr[j]>arr[j+1]){#include <bits/stdc++.h>
                swap((arr[j]),(arr[j+1]));
            }
        }
---
+++ mergeSortedArraysLeetCode.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void mergeArr(int arr1[], int n, int arr2[], int m, int arr3[])
+{
+    int i, j = 0;
+    int k = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            arr3[k++] = arr1[i++];
+        }
+        else
+        {
+            arr3[k++] = arr2[j++];
+        }
+    }
+    while (i < n)
+    {
+        arr3[k++] = arr1[i++];
+    }
+    while (j < m)
+    {
+        arr2[k++] = arr2[j++];
+    }
+}
+
+void printArr(int ans[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int arr1[5] = {1, 3, 5, 7, 9};
+    int arr2[3] = {2, 4, 6};
+    int arr3[8] = {0};
+
+    mergeArr(arr1, 5, arr2, 3, arr3);
+    printArr(arr3, 8);
+
+    return 0;
+}
