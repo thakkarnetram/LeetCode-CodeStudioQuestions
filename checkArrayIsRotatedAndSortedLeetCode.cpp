33ea2d8df885ad2dd59d1caae0b4624a21b45f4e
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-02-08 11:00:23 +0530

    8 feb 2023

---
+++ checkArrayIsRotatedAndSortedLeetCode.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool check(vector<int> &nums)
+    {
+        int cnt = 0, n = nums.size();
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i - 1] > nums[i])
+                cnt++;
+        }
+        if (nums[n - 1] > nums[0])
+            cnt++;
+        return cnt <= 1;
+    }
+};
